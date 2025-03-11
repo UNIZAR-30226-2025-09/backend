d75479d27bf9b85c6b676536731190ae418e5b8a
abdel <871208@unizar.es>
2025-03-11 03:54:40 +0100

    Borrado server.sh

--- docker/server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Ejecutar migraciones y seeders de Sequelize
-npx sequelize-cli db:migrate --config database/config/config.js
-npx sequelize-cli db:seed:all --config database/config/config.js
-
-# Iniciar el servidor
-exec npm start
