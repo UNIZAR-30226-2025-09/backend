938f261fb5f81e39eea00eae536b0a6325b67e89
870421 <870421@unizar.es>
2025-03-23 20:23:27 +0100

    nuevas canciones + nuevos inserts en seeders

--- seeders/20250304180829-datos-song-playlist.js
+++ seeders/20250304180829-datos-song-playlist.js
@@ -49,6 +49,24 @@ export async function up(queryInterface, Sequelize) {
     { song_id: 3, playlist_id: 5, date: new Date("2024-07-02") },
     { song_id: 4, playlist_id: 5, date: new Date("2024-07-02") },
     { song_id: 5, playlist_id: 5, date: new Date("2024-07-02") },
+
+      //Los 200 España
+    { song_id: 1, playlist_id: 6, date: new Date("2025-03-23") },
+    { song_id: 2, playlist_id: 6, date: new Date("2025-03-23") },
+    { song_id: 3, playlist_id: 6, date: new Date("2025-03-23") },
+    { song_id: 5, playlist_id: 6, date: new Date("2025-03-23") },
+    { song_id: 6, playlist_id: 6, date: new Date("2025-03-23") },
+    { song_id: 7, playlist_id: 6, date: new Date("2025-03-23") },
+    { song_id: 8, playlist_id: 6, date: new Date("2025-03-23") },
+    { song_id: 9, playlist_id: 6, date: new Date("2025-03-23") },
+    { song_id: 10, playlist_id: 6, date: new Date("2025-03-23") },
+    { song_id: 11, playlist_id: 6, date: new Date("2025-03-23") },
+    { song_id: 12, playlist_id: 6, date: new Date("2025-03-23") },
+    { song_id: 13, playlist_id: 6, date: new Date("2025-03-23") },
+    { song_id: 14, playlist_id: 6, date: new Date("2025-03-23") },
+    { song_id: 15, playlist_id: 6, date: new Date("2025-03-23") },
+    { song_id: 16, playlist_id: 6, date: new Date("2025-03-23") },
+
   ], {});
 }
 
