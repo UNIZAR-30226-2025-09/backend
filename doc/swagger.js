bf97b3dc743c54be8bfb775365f5d730d0493d1c
abdel <871208@unizar.es>
2025-04-01 16:36:14 +0200

    Documentacion mediante swagger

---
+++ doc/swagger.js
@@ -0,0 +1,28 @@
+// generate-swagger.js
+import swaggerJsdoc from 'swagger-jsdoc';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Obtener el directorio actual en módulos ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Mi API',
+            version: '1.0.0',
+            description: 'Documentación de la API generada con Swagger',
+        },
+    },
+    apis: ['../src/routes/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
+fs.writeFileSync(
+    path.join(__dirname, 'swagger-output.json'),
+    JSON.stringify(specs, null, 2)
+);
+console.log('Documentación de Swagger generada');
--- src/routes/artist_routes.js
+++ src/routes/artist_routes.js
@@ -4,10 +4,43 @@ import { getAllArtists } from "#src/controllers/artist_controller";
 const router = express.Router();
 
 /**
- * Rutas para la gestión de artistas:
- * - `GET /api/artists` -> Devuelve todos los artistas disponibles en la base de datos.
- *   Responde con una lista de objetos que contienen solo los campos `id`, `name` y `photo`,
- *   ordenados alfabéticamente por nombre. Si no hay artistas, responde con un mensaje de error 404.
+ * @swagger
+ * /api/artists:
+ *   get:
+ *     description: Obtiene todos los artistas.
+ *     responses:
+ *       200:
+ *         description: Lista de artistas
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: El ID del artista.
+ *                   name:
+ *                     type: string
+ *                     description: El nombre del artista.
+ *                   photo:
+ *                     type: string
+ *                     description: La URL de la foto del artista.
+ *             examples:
+ *               application/json:
+ *                 value:
+ *                   [
+ *                     {
+ *                       "id": 1,
+ *                       "name": "Artist Name",
+ *                       "photo": "url_to_photo"
+ *                     }
+ *                   ]
+ *       404:
+ *         description: No hay artistas disponibles.
+ *       500:
+ *         description: Error interno del servidor
  */
 router.get("/artists", getAllArtists);
 
