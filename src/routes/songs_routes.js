4c23d2b03a56a95c20d07ce216e28bc351f6bff2
abdel <871208@unizar.es>
2025-04-01 17:15:11 +0200

    Documentacion de artists, stripe y songs

--- src/routes/songs_routes.js
+++ src/routes/songs_routes.js
@@ -1,18 +1,106 @@
 import express from "express";
-import { getAllSongs,getSongById } from "#src/controllers/songs_controller";
+import { getAllSongs, getSongById } from "#src/controllers/songs_controller";
 
 const router = express.Router();
 
 /**
- * Rutas para la gestión de canciones:
- *
- * - `GET /api/songs/` -> Devuelve todas las canciones disponibles en la base de datos.
- *
- * - `GET /api/songs/{id}` -> Devuelve los detalles de una canción específica por su ID.
- *                            Si no se encuentra la canción, devuelve un error 404.
+ * @swagger
+ * tags:
+ *   - name: Songs
+ *     description: Operaciones relacionadas con las canciones.
  */
 
+/**
+ * @swagger
+ * /api/songs:
+ *   get:
+ *     tags:
+ *       - Songs
+ *     description: Devuelve todas las canciones disponibles en la base de datos.
+ *     responses:
+ *       200:
+ *         description: Lista de canciones
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: El ID de la canción.
+ *                   name:
+ *                     type: string
+ *                     description: El nombre de la canción.
+ *                   duration:
+ *                     type: integer
+ *                     description: La duración de la canción en segundos.
+ *                   lyrics:
+ *                     type: string
+ *                     description: Las letras de la canción.
+ *                   photo_video:
+ *                     type: string
+ *                     description: La URL de la foto o video relacionado con la canción.
+ *                   url_mp3:
+ *                     type: string
+ *                     description: La URL del archivo MP3 de la canción.
+ *                   genre:
+ *                     type: string
+ *                     description: El género de la canción.
+ *       500:
+ *         description: Error interno del servidor
+ */
 router.get("/", getAllSongs);
+
+/**
+ * @swagger
+ * /api/songs/{id}:
+ *   get:
+ *     tags:
+ *       - Songs
+ *     description: Devuelve los detalles de una canción específica por su ID. Si no se encuentra la canción, devuelve un error 404.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: ID de la canción.
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Detalles de la canción
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   description: El ID de la canción.
+ *                 name:
+ *                   type: string
+ *                   description: El nombre de la canción.
+ *                 duration:
+ *                   type: integer
+ *                   description: La duración de la canción en segundos.
+ *                 lyrics:
+ *                   type: string
+ *                   description: Las letras de la canción.
+ *                 photo_video:
+ *                   type: string
+ *                   description: La URL de la foto o video relacionado con la canción.
+ *                 url_mp3:
+ *                   type: string
+ *                   description: La URL del archivo MP3 de la canción.
+ *                 genre:
+ *                   type: string
+ *                   description: El género de la canción.
+ *       404:
+ *         description: No se encontró la canción con el ID especificado.
+ *       500:
+ *         description: Error interno del servidor
+ */
 router.get("/:id", getSongById);
 
 export default router;
