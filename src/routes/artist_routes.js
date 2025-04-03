f4cd89d5e52154884931f4470c3b05922aaf4b5c
abdel <871208@unizar.es>
2025-04-03 14:50:21 +0200

    Documentacion artista

--- src/routes/artist_routes.js
+++ src/routes/artist_routes.js
@@ -47,6 +47,114 @@ const router = express.Router();
  */
 router.get("/artists", artistController.getAllArtists);
 
+/**
+ * @swagger
+ * /api/artist/{artistId}:
+ *   get:
+ *     tags:
+ *       - Artists
+ *     description: Obtiene los detalles de un artista específico, sus canciones populares, álbumes y sencillos.
+ *     parameters:
+ *       - in: path
+ *         name: artistId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del artista
+ *     responses:
+ *       200:
+ *         description: Detalles del artista
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 artist:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       description: ID del artista
+ *                     name:
+ *                       type: string
+ *                       description: Nombre del artista
+ *                     bio:
+ *                       type: string
+ *                       description: Biografía del artista
+ *                     photo:
+ *                       type: string
+ *                       description: URL de la foto del artista
+ *                 songs:
+ *                   type: array
+ *                   description: Lista de las canciones más populares (máximo 5)
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                       name:
+ *                         type: string
+ *                       duration:
+ *                         type: string
+ *                       photo_video:
+ *                         type: string
+ *                       type:
+ *                         type: string
+ *                       url_mp3:
+ *                         type: string
+ *                       likes:
+ *                         type: integer
+ *                 albums:
+ *                   type: array
+ *                   description: Álbumes del artista
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                       name:
+ *                         type: string
+ *                       front_page:
+ *                         type: string
+ *                 singles:
+ *                   type: array
+ *                   description: Canciones de tipo sencillo
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                       name:
+ *                         type: string
+ *                       duration:
+ *                         type: string
+ *                       photo_video:
+ *                         type: string
+ *                       type:
+ *                         type: string
+ *                       url_mp3:
+ *                         type: string
+ *       404:
+ *         description: Artista no encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Artista no encontrado
+ *       500:
+ *         description: Error interno del servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Error interno del servidor
+ */
 router.get("/:artistId", artistController.getArtistDetails);
 
 export default router;
