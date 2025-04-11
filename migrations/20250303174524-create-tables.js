79d54ae6f8cabfe0d3e1282244642a6ebda025ce
gaxur <874095@unizar.es>
2025-04-11 13:11:46 +0200

    Funciona

--- migrations/20250303174524-create-tables.js
+++ migrations/20250303174524-create-tables.js
@@ -17,7 +17,7 @@ export default {
       user_picture: { type: Sequelize.STRING, allowNull: true },
       created_at: { type: Sequelize.DATE, defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"), allowNull: false },
       reset_token: { type: Sequelize.STRING, allowNull: true },
-      reset_token_expires: { type: Sequelize.DATE, allowNull: true },
+      reset_token_expires: { type: Sequelize.DATE, allowNull: true }
     });
 
     // Tabla ARTIST
