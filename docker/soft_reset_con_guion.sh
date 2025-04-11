bb7c80982ba8f358aedc557fd8f6ddd30239bbaa
abdel <871208@unizar.es>
2025-04-11 15:50:34 +0200

    Nuevos scripts de docker para diferenciar entre las versiones de docker que usan docker-compose y las que usan docker compose

---
+++ docker/soft_reset_con_guion.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Deteniendo contenedores..."
+docker-compose down
+
+echo "Reconstruyendo imágenes..."
+docker-compose build
+
+echo "Iniciando contenedores en modo detached..."
+docker-compose up -d
+
+echo "¡Soft reset completado! Los contenedores están ejecutándose en segundo plano."
